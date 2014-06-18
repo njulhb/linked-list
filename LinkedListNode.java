d1141c3a2105c44fad0504398ef9cb957dc34533
lhb <njulinhaibo@163.com>
2014-06-18 18:56:59 +0800

    1.1

--- LinkedListNode.java
+++ LinkedListNode.java
@@ -3,12 +3,13 @@ public class LinkedListNode {
 	LinkedListNode next = null;
 	int data;
 	public LinkedListNode(int d){data = d;}
-	void appendToTail(int d)
+	public LinkedListNode appendToTail(int d)
 	{
 		LinkedListNode end = new LinkedListNode(d);
 		LinkedListNode n = this;
 		while(n.next != null){n = n.next;}
 		n.next = end;
+		return end;
 	}
 	LinkedListNode deleteNode(LinkedListNode head, int d)
 	{
@@ -28,15 +29,34 @@ public class LinkedListNode {
 		}
 		return head;
 	}
-	LinkedListNode FindBeginningLoop(LinkedListNode head)
+	static LinkedListNode FindBeginningLoop(LinkedListNode head) 
 	{
 		HashMap<LinkedListNode,Boolean> map = new HashMap<LinkedListNode,Boolean>();
 		while(head != null)
 		{
-			if(map.get(head))return head;
+			if(map.containsKey(head))
+				return head;
 			else
+			{
 				map.put(head,true);
+				head = head.next;
+			}
 		}
 		return head;
+	}
+	public static void main(String[] args)
+	{
+		LinkedListNode n = new LinkedListNode(0);
+		LinkedListNode n1 = null;
+		LinkedListNode n2 = null;
+		for(int i = 1; i < 15; i++)
+		{		
+			n1 = n.appendToTail(i);
+			if(i == 5)
+				n2 = n1;
+		}
+		n1.next = n2;
+		n1 = FindBeginningLoop(n);
+		System.out.println(n1.data);
 	}
 }
